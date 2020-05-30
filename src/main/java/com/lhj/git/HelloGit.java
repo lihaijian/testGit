2d39310353f4b864bc4700a71fdea20cdb5783f0
lihaijian <306860856@qq.com>
2020-05-30 23:05:03 +0800

    创建分支

--- src/main/java/com/lhj/git/HelloGit.java
+++ src/main/java/com/lhj/git/HelloGit.java
@@ -4,6 +4,8 @@ public class HelloGit {
 	public static void main(String[] args){
 		System.out.println("Hello,Git");
 		System.out.println("更新1");
+		System.out.println("创建分支");
 		System.out.println("更新2");
+		System.out.println("创建分支");
 	}
 }
